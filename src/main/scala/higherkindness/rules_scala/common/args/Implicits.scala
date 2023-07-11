2e663bdcbadd6eba040b74c432c0972e310cc29b
James Judd <james@lucidchart.com>
2023-07-11 14:23:49 -0600

    Update more things to Scala 3

--- src/main/scala/higherkindness/rules_scala/common/args/Implicits.scala
+++ src/main/scala/higherkindness/rules_scala/common/args/Implicits.scala
@@ -7,7 +7,7 @@ import net.sourceforge.argparse4j.inf.Argument
 object implicits {
   implicit final class SetArgumentDefault(val argument: Argument) extends AnyVal {
     // https://issues.scala-lang.org/browse/SI-2991
-    private[this] type SetDefault = { def setDefault(value: AnyRef) }
+    private[this] type SetDefault = { def setDefault(value: AnyRef): Unit }
     def setDefault_[A](value: A) = argument.asInstanceOf[SetDefault].setDefault(value.asInstanceOf[AnyRef])
   }
 }

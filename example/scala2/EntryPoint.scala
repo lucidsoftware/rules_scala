e470c7ee47204ccc4616b4afe0582afd9870a0be
Jaden Peterson <jpeterson@lucidchart.com>
2023-07-12 12:44:47 -0400

    Added example Scala 2 and Scala 3 targets for debugging

---
+++ example/scala2/EntryPoint.scala
@@ -0,0 +1,7 @@
+package com.lucidchart.example.scala2
+
+import com.lucidchart.example.scala3.Test
+
+object EntryPoint {
+  @main def main(): Unit = Test.printHelloWorld()
+}
---
+++ example/scala3/Test.scala
@@ -0,0 +1,5 @@
+package com.lucidchart.example.scala3
+
+object Test {
+  def printHelloWorld(): Unit = println("Hello, world!")
+}

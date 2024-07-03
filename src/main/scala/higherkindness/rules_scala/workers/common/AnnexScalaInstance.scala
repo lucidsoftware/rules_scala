9d76dfd2b690a4046c160c8e58920c1a2e7f8cdf
James Judd <james@lucidchart.com>
2024-07-02 23:05:10 -0600

    Get ClassLoaders figured out for AnnexScalaInstance
    The ClassLoaders were mostly correct before, but this should get things
    the rest of the way to being correct. Or at least similarly correct to
    how Zinc and Bloop as this is very similar to how they set up their
    classloaders.

--- src/main/scala/higherkindness/rules_scala/workers/common/AnnexScalaInstance.scala
+++ src/main/scala/higherkindness/rules_scala/workers/common/AnnexScalaInstance.scala
@@ -1,28 +1,77 @@
 package higherkindness.rules_scala
 package workers.common
 
-import xsbti.compile.{CompilerInterface2, ScalaInstance}
+import xsbti.compile.ScalaInstance
 import java.io.File
 import java.net.URLClassLoader
 import java.util.Properties
 
+object AnnexScalaInstance {
+  // Check the comment on ScalaInstanceDualLoader to understand why this is necessary.
+  private val dualClassLoader: ScalaInstanceDualLoader = {
+    new ScalaInstanceDualLoader(
+      // Classloader for xsbti classes
+      getClass.getClassLoader,
+      // Classloader for all other classes
+      ClassLoader.getPlatformClassLoader(),
+    )
+  }
+
+  /**
+   * Convenience function to get a classloader from an Array[File] whose parent
+   * is the dualClassLoader, so it will handle xsbti correctly.
+   */
+  private def getClassLoader(jars: Array[File]): URLClassLoader = {
+    new URLClassLoader(
+      jars.map(_.toURI.toURL),
+      dualClassLoader,
+    )
+  }
+
+  // These are not the most robust checks for these jars, but it is more or
+  // less what Zinc and Bloop are doing. They're also fast, so if it works it
+  // works.
+
+  private final def isScala2CompilerJar(jarName: String): Boolean = {
+    jarName.startsWith("scala-compiler") || jarName.startsWith("scala-reflect")
+  }
+
+  private final def isScala3CompilerJar(jarName: String): Boolean = {
+    jarName.startsWith("scala3-compiler") || jarName.startsWith("scala3-interfaces") ||
+      jarName.startsWith("tasty-core_3") || jarName.startsWith("scala-asm")
+  }
+
+  private final def isScalaLibraryJar(jarName: String): Boolean = {
+    jarName.startsWith("scala-library") || jarName.startsWith("scala3-library")
+  }
+}
+
+// See this interface for comments better explaining the purpose of all the
+// member variables:
+// https://github.com/sbt/zinc/blob/4eacff2a9bf5c8750bfc5096955065ce67f4e68a/internal/compiler-interface/src/main/java/xsbti/compile/ScalaInstance.java
 final class AnnexScalaInstance(override val allJars: Array[File]) extends ScalaInstance {
-  // Some of these variable names don't really line up with what I imagine the
-  // actual values should be. I'm following the examples I found from the Zinc source
-  // and it seems to work well, so I'm sticking with it.
-  // For reference: https://github.com/sbt/zinc/blob/1.5.x/internal/zinc-classpath/src/main/scala/sbt/internal/inc/ScalaInstance.scala
-  override val compilerJars: Array[File] = allJars
 
-  override val libraryJars: Array[File] = allJars
-    .filter(f => new URLClassLoader(Array(f.toURI.toURL)).findResource("library.properties") != null)
+  // Jars for the Scala compiler classes
+  // We need to include the full classpath for the Scala 2 or Scala 3 compilers.
+  // Thankfully that classpath doesn't seem to change very often.
+  override val compilerJars: Array[File] = allJars.filter { jar =>
+    val jarName = jar.getName
+    AnnexScalaInstance.isScala2CompilerJar(jarName) ||
+      AnnexScalaInstance.isScala3CompilerJar(jarName) ||
+      AnnexScalaInstance.isScalaLibraryJar(jarName)
+  }
 
+  // Jars for the Scala library classes
+  override val libraryJars: Array[File] =
+    allJars.filter(jar => AnnexScalaInstance.isScalaLibraryJar(jar.getName))
+
+  // All the jars that are not compiler or library jars
   override val otherJars: Array[File] = allJars.diff(compilerJars ++ libraryJars)
 
+  // Version for this Scala instance
   override val actualVersion: String = {
-    val stream = new URLClassLoader(
-      compilerJars.map(_.toURI.toURL),
-      ClassLoader.getPlatformClassLoader()
-    ).getResourceAsStream("compiler.properties")
+    val stream = AnnexScalaInstance.getClassLoader(compilerJars)
+      .getResourceAsStream("compiler.properties")
 
     try {
       val props = new Properties
@@ -32,44 +81,58 @@ final class AnnexScalaInstance(override val allJars: Array[File]) extends ScalaI
   }
   override val version: String = actualVersion
 
-  override val loaderLibraryOnly: ClassLoader =
-    new URLClassLoader(
-      libraryJars.map(_.toURI.toURL),
-      // The Scala 3 compiler jar has a dependency on the org.scala-sbt:compiler-interface jar.
-      // Our ZincRunner code also has a dependency on the compiler-interface.
-      //
-      // This means we will end up in a situation with classes from the compiler-interface
-      // being loaded by the classloader for the jvm that launched ZincRunner and also
-      // the classloader that we're creating here for the Scala 3 compiler.
-      //
-      // This is a problem.
-      //
-      // An instance of a class in the JVM is defined by its fully qualified name
-      // and classloader. Because the classloaders are different, we will get a
-      // java.lang.LinkageError because the classloader for the ZincRunner will have already
-      // loaded some compiler-interface classes that get used by the Scala 3 compiler during
-      // its compilation run.
-      //
-      // To avoid this we are setting the parent of the Scala 3 compiler classloader, which
-      // we are creating here, to the classloader for the jvm that launched the ZincRunner.
-      // That avoids the LinkageError because URLClassLoaders go parent first, so it finds
-      // the compiler-interface classes from the correct classloader.
-      //
-      // Problem is, doing this creates a potential leak: the parent classloader will be checked
-      // for all classes used during the compilation. Meaning, any classes used in the
-      // ZincRunner could end up leaking out into the Scala 3 compiler. So far this hasn't
-      // caused any issues, but maybe it will in the future.
-      //
-      // Scala 2's compiler does not have the dependency on the compiler-interface, so it avoids
-      // this problem.
-      //
-      // TODO: Fix this with classloader wizardry and witchcraft
-      if (actualVersion.startsWith("3")) classOf[CompilerInterface2].getClassLoader
-      else ClassLoader.getPlatformClassLoader()
-    )
+  // Loader for only the classes and resources in the library jars of this Scala instance
+  override val loaderLibraryOnly: ClassLoader = AnnexScalaInstance.getClassLoader(libraryJars)
 
-  override val loader: ClassLoader =
-    new URLClassLoader(allJars.diff(libraryJars).map(_.toURI.toURL), loaderLibraryOnly)
+  // Loader for all the classes and resources in all the jars of this Scala instance
+  override val loader: ClassLoader = AnnexScalaInstance.getClassLoader(allJars)
 
-  override val loaderCompilerOnly: ClassLoader = loader
+  // Loader for all the classes and resources in all the compiler jars of this
+  // Scala instance
+  override val loaderCompilerOnly: ClassLoader = AnnexScalaInstance.getClassLoader(compilerJars)
+}
+
+/**
+ * Load classes that start with xsbti or jline from the sbt class loader
+ *   - xsbti is for the compiler bridge
+ *   - jline for the sbt provided jline terminal
+ * Load all other classes from the other loader
+ * Always get resources from the non-sbt loader
+ *
+ * This is necessary, so we can always make sure that the xsbti and jline
+ * classes needed for compilation and the repl come from the same class loader.
+ *
+ * If the same class is loaded by two separate class loaders you get a linkage
+ * error in the JVM.
+ *
+ * If you set up a class loader that doesn't contain the xsbti and jline
+ * classes, then you get an error when you can't find those classes because
+ * they are needed by Zinc and our ZincRunner.
+ *
+ * If you set up one class loader for all the things, then you start leaking
+ * any class loaded during any compilation into any compilation happening in
+ * the JVM. Considering workers are long lived and handle multiple compilations,
+ * that could become a problem.
+ *
+ * Dotty, Bloop, and Zinc all do very similar things. This approach was heavily
+ * inspired by their approaches. For more details about their approaches see
+ * the following:
+ * https://github.com/scala/scala3/blob/7d26a96db6e63aba8db1362b09f4adf521bc8327/sbt-dotty/src/dotty/tools/sbtplugin/DottyPlugin.scala#L620
+ * https://github.com/scala/scala3/blob/cd8c5ed1dc5ee8528861e284490534064ba6d3e5/sbt-bridge/src/xsbt/CompilerClassLoader.java#L11
+ * https://github.com/scalacenter/bloop/blob/c505385edf0bbd420e19401ae1beabae8895df8f/backend/src/main/scala/bloop/ScalaInstanceTopLoader.scala#L30
+ * https://github.com/sbt/zinc/blob/4eacff2a9bf5c8750bfc5096955065ce67f4e68a/internal/zinc-classpath/src/main/scala/sbt/internal/inc/classpath/DualLoader.scala#L47
+ */
+class ScalaInstanceDualLoader(sbtClassLoader: ClassLoader, otherLoader: ClassLoader)
+    extends ClassLoader(otherLoader) {
+  override final def loadClass(name: String, resolve: Boolean): Class[_] = {
+    if (name.startsWith("xsbti.") || name.startsWith("org.jline.")) {
+      val klass = sbtClassLoader.loadClass(name)
+      if (resolve) {
+        resolveClass(klass)
+      }
+      klass
+    } else {
+      super.loadClass(name, resolve)
+    }
+  }
 }

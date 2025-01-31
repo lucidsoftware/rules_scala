2f73ccd46d3d603aee918385ed0988632658a568
Ryan Ferguson <ryan28561@gmail.com>
2025-01-31 10:36:46 -0700

    real cache bust this time

--- src/main/scala/higherkindness/rules_scala/workers/zinc/compile/ZincRunner.scala
+++ src/main/scala/higherkindness/rules_scala/workers/zinc/compile/ZincRunner.scala
@@ -39,13 +39,13 @@ class ZincRunnerWorkerConfig private (
 object ZincRunnerWorkerConfig {
   def apply(namespace: Namespace): ZincRunnerWorkerConfig = {
     new ZincRunnerWorkerConfig(
-      pathFrom("persistence_dir", namespace),
+      pathFromBust("persistence_dir", namespace),
       Option(namespace.getBoolean("use_persistence")).map(Boolean.unbox).getOrElse(false),
-      pathFrom("extracted_file_cache", namespace),
+      pathFromBust("extracted_file_cache", namespace),
     )
   }
 
-  private def pathFrom(arg: String, namespace: Namespace): Option[Path] = {
+  private def pathFromBust(arg: String, namespace: Namespace): Option[Path] = {
     Option(namespace.getString(arg)).map { pathString =>
       if (pathString.startsWith("~" + File.separator)) {
         Paths.get(pathString.replace("~", sys.props.getOrElse("user.home", "")))

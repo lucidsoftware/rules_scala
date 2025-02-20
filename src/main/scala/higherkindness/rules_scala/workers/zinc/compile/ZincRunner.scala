289686d6e3be047758ea69a9ade8f20c2e9cc25d
Ryan Ferguson <ryan28561@gmail.com>
2025-02-20 13:51:10 -0700

    last one for reals this time

--- src/main/scala/higherkindness/rules_scala/workers/zinc/compile/ZincRunner.scala
+++ src/main/scala/higherkindness/rules_scala/workers/zinc/compile/ZincRunner.scala
@@ -39,13 +39,13 @@ class ZincRunnerWorkerConfig private (
 object ZincRunnerWorkerConfig {
   def apply(namespace: Namespace): ZincRunnerWorkerConfig = {
     new ZincRunnerWorkerConfig(
-      pathFromBustRBEBust5("persistence_dir", namespace),
+      pathFromBustBustRBEBust5("persistence_dir", namespace),
       Option(namespace.getBoolean("use_persistence")).map(Boolean.unbox).getOrElse(false),
-      pathFromBustRBEBust5("extracted_file_cache", namespace),
+      pathFromBustBustRBEBust5("extracted_file_cache", namespace),
     )
   }
 
-  private def pathFromBustRBEBust5(arg: String, namespace: Namespace): Option[Path] = {
+  private def pathFromBustBustRBEBust5(arg: String, namespace: Namespace): Option[Path] = {
     Option(namespace.getString(arg)).map { pathString =>
       if (pathString.startsWith("~" + File.separator)) {
         Paths.get(pathString.replace("~", sys.props.getOrElse("user.home", "")))
